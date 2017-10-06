b3eed9be694ebdea40e48d2438d99cd8e5397188
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-06 08:38:11 -0400

    unity camera example

---
+++ unity/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Transform transformOrig;
+
+	private void Start () {
+        transformOrig = transform;
+	}
+
+    public void reset() {
+        transform.position = transformOrig.position;
+        transform.rotation = transformOrig.rotation;
+        transform.localScale = transformOrig.localScale;
+    }
+
+}
---
+++ unity/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public GameManager gm;
+    public BasicController ctl;
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) gm.reset();
+
+        if (ctl.clicked && ctl.isLooking && ctl.isLookingAt.StartsWith("Base")) {
+            GameObject.Find(ctl.isLookingAt).GetComponent<Base>().doIsVisited();
+        }
+	}
+}
