7583731e3216e9c4e7802144ded9bf45d389c186
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-07 00:12:07 -0400

    continue p5.js version

--- p5js/js/cam.js
+++ p5js/js/cam.js
@@ -1,1 +1,117 @@
 "use strict";
+
+// https://processing.org/reference/camera_.html
+class Cam {
+
+    constructor() {
+        this.pos = createVector(0,0,0);
+        this.poi = createVector(0,0,0);
+        this.up = createVector(0,0,0);
+
+        this.mouse = createVector(0,0,0);
+        this.p3d;
+        this.proj;
+        this.cam;
+        this.modvw;
+        this.modvwInv;
+        this.screen2Model;
+        
+        this.displayText = "";
+        this.font;
+        this.fontSize = 12;
+
+        this.defaultPos();
+        this.defaultPoi();
+        this.defaultUp();
+        this.init();
+    }
+
+    init() {
+        this.p3d = g;
+        //proj = new PMatrix3D();
+        this.cam = new PMatrix3D();
+        //modvw = new PMatrix3D();
+        this.modvwInv = new PMatrix3D();
+        this.screen2Model = new PMatrix3D();
+        
+        this.font = createFont("Arial", this.fontSize);
+    }
+    
+    screenToWorldCoords(p) {
+        //proj = p3d.projection.get();
+        this.cam = this.p3d.modelview.get(); //camera.get();
+        //modvw = p3d.modelview.get();
+        this.modvwInv = this.p3d.modelviewInv.get();
+        this.screen2Model = this.modvwInv;
+        this.screen2Model.apply(this.cam);
+        var screen = [ this.p.x, this.p.y, this.p.z ];
+        var model = [ 0, 0, 0 ];
+        model = this.screen2Model.mult(screen, model);
+        
+        return createVector(model[0] + (this.poi.x - width/2), model[1] + (this.poi.y - height/2), model[2]);
+    }
+    
+    screenToWorldMouse() {
+        this.mouse = screenToWorldCoords(createVector(mouseX, mouseY, this.poi.z));
+    }
+    
+    update() {
+        this.screenToWorldMouse();
+    }
+    
+    draw() {
+        camera(this.pos.x, this.pos.y, this.pos.z, this.poi.x, this.poi.y, this.poi.z, this.up.x, this.up.y, this.up.z);
+        this.drawText();
+    }
+    
+    run() {
+        this.update();
+        this.draw();
+    }
+    
+    move(x, y, z) {
+        var p = createVector(x,y,z);
+        this.pos = this.pos.add(p);
+        this.poi = this.poi.add(p);
+    }
+    
+    defaultPos() {
+        this.pos.x = width/2.0;
+        this.pos.y = height/2.0;
+        this.pos.z = (height/2.0) / tan(PI*30.0 / 180.0);
+    }
+    
+    defaultPoi() {
+        this.poi.x = width/2.0;
+        this.poi.y = height/2.0;
+        this.poi.z = 0;
+    }
+    
+    defaultUp() {
+     this.up.x = 0;
+     this.up.y = 1;
+     this.up.z = 0;
+    }
+    
+    reset() {
+     this.defaultPos();
+     this.defaultPoi();
+     this.defaultUp();
+    }
+    
+    drawText() {
+        if (!this.displayText.equals("")) {
+            push();    
+            translate((this.pos.x - (width/2)) + (this.fontSize/2), (this.pos.y - (height/2)) + this.fontSize, this.poi.z);
+            textFont(this.font, this.fontSize);
+            text(this.displayText, 0, 0);
+            pop();
+        }
+    }
+    
+}
+
+// TODO
+// https://processing.org/reference/frustum_.html
+// https://processing.org/reference/beginCamera_.html
+// https://processing.org/reference/endCamera_.html
--- p5js/js/main.js
+++ p5js/js/main.js
@@ -1,1 +1,57 @@
 "use strict";
+
+var cam, cubes;
+
+function setup() {
+  createCanvas(1280, 720, WEBGL);
+  cam = new Cam();
+  cubes = [];
+  noStroke();
+}
+
+function draw() {
+  lights();
+  background(0);
+
+  push();
+  translate(width/2, height/2, 0);
+  rotateX(-PI/6);
+  rotateY(PI/3);
+  box(100);
+  pop();
+  
+  updateControls();
+  cam.run();
+  
+  for (var i=0; i<cubes.size(); i++) {
+    cubes[i].run();
+  }
+}
+
+function mousePressed() {
+  cubes.push(new Cube());
+}
+
+
+class Cube {
+  
+  constructor() {
+    this.pos = cam.mouse;
+    this.rot = createVector(-PI/random(3,6), PI/random(3,6), 0);
+  }
+  
+  run() {
+    push();
+    translate(this.pos.x, this.pos.y, this.pos.z);
+    rotateXYZ(this.rot.x, this.rot.y, this.rot.z);
+    box(10);
+    pop();
+  }
+  
+  rotateXYZ(x, y, z){
+    rotateX(x);
+    rotateY(y);
+    rotateZ(z);
+  }
+  
+}
